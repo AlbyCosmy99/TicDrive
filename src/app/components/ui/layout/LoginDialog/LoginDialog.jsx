51e73fe825ddc58e7dc5526029945da7e2c7bad0
Andrei <dinamo1999@icloud.com>
2024-06-01 17:43:30 +0200

    changes

---
+++ src/app/components/ui/layout/LoginDialog/LoginDialog.jsx
@@ -0,0 +1,36 @@
+import AuthContext from '@/app/Contexts/AuthContext';
+import { useContext } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export default function LoginDialog() {
+  const { isLoginDialog, setIsLoginDialog } = useContext(AuthContext);
+
+  const handleClose = () => {
+    setIsLoginDialog(false);
+  };
+
+  return (
+    <>
+      <Modal
+        show={isLoginDialog}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Login form placeholder</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Login</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
--- src/app/components/ui/layout/Navbar/NavbarApp.jsx
+++ src/app/components/ui/layout/Navbar/NavbarApp.jsx
@@ -1,19 +1,27 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import Link from 'next/link';
+import { useState } from "react";
+import AuthContext from "@/app/Contexts/AuthContext";
+import LoginDialog from "../LoginDialog/LoginDialog";
 
 export default function NavbarApp() {
+    const [isLoginDialog, setIsLoginDialog] = useState(false);
+
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="/">TIC DRIVE</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto d-flex gap-3">
-                        <Link href="/login">LOGIN</Link>
-                        <Link href="/register">REGISTRATI ORA</Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+        <AuthContext.Provider value={{ isLoginDialog, setIsLoginDialog }}>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="/">TIC DRIVE</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto d-flex gap-3">
+                            <Button onClick={() => setIsLoginDialog(true)}>Login</Button>
+                            <Button>REGISTRATI ORA</Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            {isLoginDialog && <LoginDialog />}
+        </AuthContext.Provider>
+    );
 }
